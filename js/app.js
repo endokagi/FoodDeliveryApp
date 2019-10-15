a22eb94e71deb2fe206a6fa1ad48199e6db5a3bb
endokagi <38246228+endokagi@users.noreply.github.com>
2019-10-16 00:57:56 +0700

    add more thing edit more thing

--- js/app.js
+++ js/app.js
@@ -1,17 +1,38 @@
-// Login
-var login = function () {
-  var username = document.getElementById('username').value;
-  var password = document.getElementById('password').value;
-
-  if (username === 'thewlip' && password === '0000') {
-    // ons.notification.alert('Congratulations!');
-    window.location.replace("main.html");}
-  else if (username === 'w' && password === 'w') {
-    window.location.replace("main.html");
-  } else {
-    ons.notification.alert('Incorrect username or password.');
-  }
+//////////////////////// Firebase //////////////////////////////////
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyCNGyFiG2de8IyC0TUESCmiaqWhxWGTDKs",
+  authDomain: "food-delivery-app-c4284.firebaseapp.com",
+  databaseURL: "https://food-delivery-app-c4284.firebaseio.com",
+  projectId: "food-delivery-app-c4284",
+  storageBucket: "food-delivery-app-c4284.appspot.com",
+  messagingSenderId: "704396540917",
+  appId: "1:704396540917:web:265c9616397bfd760c7ebc"
 };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// Use firestorengin
+var db = firebase.firestore();
+
+// Check Firebase Authentication
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+    // User is signed in.
+    var displayName = user.displayName;
+    var email = user.email;
+    console.log(email + " sign in");
+    var emailVerified = user.emailVerified;
+    var photoURL = user.photoURL;
+    var isAnonymous = user.isAnonymous;
+    var uid = user.uid;
+    var providerData = user.providerData;
+    ons.notification.alert("Login Sucess !");
+    $("#content")[0].load("foodCategory.html");
+    // ...
+  } else {
+
+  }
+});
 
 // Regist
 var regist = function () {
@@ -32,22 +53,97 @@ var regist = function () {
 };
 
 // backtologin
-var backtologin = function () {
-  $("#Register").click(function () {
-    window.location.replace("index.html");
-  });
-}
+$("#Register").click(function () {
+  $("#content")[0].load("login.html");
+});
+
 
 // main
 document.addEventListener('init', function (event) {
   var page = event.target;
-  console.log(page.id);
+  console.log("run "+page.id);
+
+  $("#menubtn").click(function () {
+    $("#sidemenu")[0].open();
+  });
 
   if (page.id === 'tabbar') {
     $('#menubtn').click(function () {
       var menu = document.getElementById('menu');
       menu.open();
     });
+  }
+
+  if (page.id === "loginPage") {
+    
+    // Login
+    $("#loginbtn").click(function () {
+      var username = $("#username").val();
+      var password = $("#password").val();
+
+      console.log(username, password);
+
+      firebase.auth().signInWithEmailAndPassword(username, password).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+        console.log(errorCode, errorMessage);
+      });
+
+
+    });
+
+    // login with google
+    var provider = new firebase.auth.GoogleAuthProvider();
+    $("#googlebtn").click(function () {
+
+      // popup
+      firebase.auth().signInWithPopup(provider).then(function (result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
+        $("#content")[0].load("foodCategory.html");
+      }).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
+
+    });
+
+    $("#backbtn").click(function(){
+      $("#content")[0].load("home.html");
+    });
+  }
+
+  if (page.id === "menuPage") {
+    
+    $("#login").click(function () {
+      console.log('loginbtn pressed');
+      document.querySelector('#myNavigator').pushPage('login.html');
+      $("#sidemenu")[0].close();
+
+    });
+    $("#logout").click(function () {
+      console.log('logoutbtn pressed');
+      $("#sidemenu")[0].close();
+      firebase.auth().signOut().then(function () {
+        // Sign-out successful.
+        ons.notification.alert("LogOut Sucess !");
+        $("#content")[0].load("login.html");
+      }).catch(function (error) {
+        // An error happened.
+      });
+    });
+
   }
 });
 
